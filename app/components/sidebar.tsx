85a8b5b5a9cb03a87bbdf921eca235bd93ec67b3
Light94pro <zywdegamemail@126.com>
2024-06-19 19:28:05 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           Light94pro
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          逃离电子荒原.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
